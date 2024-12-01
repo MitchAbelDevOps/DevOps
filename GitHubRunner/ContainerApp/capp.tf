08538b9d4aa22ed61ac48e2f6e49c27af81d331d
Mitch Abel <mitch.abel@adaptiv.nz>
2024-12-01 16:11:44 +1300

    Added container app initial IAC for container app environment

---
+++ GitHubRunner/ContainerApp/capp.tf
@@ -0,0 +1,37 @@
+/**************************************************
+Existing Resources
+***************************************************/
+data "azurerm_log_analytics_workspace" "log_analytics_workspace" {
+  name                = "log-${var.resourceSuffix}-${var.environment}-${var.locationSuffix}"
+  resource_group_name = local.fullResourceGroupName
+}
+
+data "azurerm_virtual_network" "tewheke_vnet" {
+  name                = "vnet-integration-${var.resourceSuffix}-${var.environment}-${var.locationSuffix}"
+  resource_group_name = "${var.networkingResourceGroupName}-${var.resourceSuffix}-${var.environment}-${var.locationSuffix}"
+}
+
+data "azurerm_subnet" "runners_subnet" {
+  name                 = "snet-runners-${var.resourceSuffix}-${var.environment}-${var.locationSuffix}"
+  resource_group_name  = "${var.networkingResourceGroupName}-${var.resourceSuffix}-${var.environment}-${var.locationSuffix}"
+  virtual_network_name = "vnet-integration-${var.resourceSuffix}-${var.environment}-${var.locationSuffix}"
+}
+
+/**************************************************
+New Resources
+***************************************************/
+// Container App Environment
+resource "azurerm_container_app_environment" "container_app_environment" {
+  // Change name to have 01 suffix
+  name                       = "cae-${var.resourceSuffix}-${var.environment}-${var.locationSuffix}"
+  location                   = var.location
+  resource_group_name        = local.fullResourceGroupName
+  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.log_analytics_workspace.id
+  workload_profile {
+    name = "consmption-workload"
+    workload_profile_type = "Consumptions"
+    maximum_count = 2
+    minimum_count = 1
+  }
+  infrastructure_subnet_id = data.azurerm_subnet.runners_subnet.id
+}
