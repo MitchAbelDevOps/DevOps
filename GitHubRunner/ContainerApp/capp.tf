9f7ce8a73fcd09770820a3a2bb494abfceda5547
Mitch Abel <mitch.abel@adaptiv.nz>
2024-12-02 17:43:14 +1300

    Added admin enabled to ACR

--- GitHubRunner/ContainerApp/capp.tf
+++ GitHubRunner/ContainerApp/capp.tf
@@ -26,6 +26,7 @@ resource "azurerm_container_registry" "container_registry" {
   location            = var.location
   resource_group_name = local.fullResourceGroupName
   sku                 = "Basic"
+  admin_enabled = true
 }
 
 // Container App Environment
